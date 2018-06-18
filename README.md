f114ca833de895840031df0add41501b6142af3b
Ezequiel Ventura <dezequielventura@yahoo.com.ar>
2018-06-18 17:08:05 -0300

    Added parenthesis to parameterless methods.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # scalajs-navigo
 
-Scala.js facade for [![Navigo](https://github.com/krasimir/navigo)]: A
+Scala.js facade for [Navigo](https://github.com/krasimir/navigo): A
 simple vanilla JavaScript router with a fallback for older browsers. 
--- src/main/scala/navigo/Navigo.scala
+++ src/main/scala/navigo/Navigo.scala
@@ -13,15 +13,15 @@ class Navigo(root: String = "", useHash: Boolean = false, hash: String = "#")
   def off(handler: js.Function): Navigo = js.native
   def navigate(path: String, absolute: Boolean): Navigo = js.native
   def resolve(url: String): Boolean = js.native
-  def resolve: Navigo = js.native
-  def destroy: Unit = js.native
+  def resolve(): Navigo = js.native
+  def destroy(): Unit = js.native
   def link(path: String): String = js.native
   def pause(pause: Boolean): Unit = js.native
-  def resume: Unit = js.native
-  def disableIfAPINotAvailable: Unit = js.native
-  def updatePageLinks: Unit = js.native
+  def resume(): Unit = js.native
+  def disableIfAPINotAvailable(): Unit = js.native
+  def updatePageLinks(): Unit = js.native
   def notFound(handler: js.Function): Navigo = js.native
-  def lastRouteResolved: String = js.native
-  def getLinkPath: String = js.native
-  def historyAPIUpdateMethod: String = js.native
+  def lastRouteResolved(): String = js.native
+  def getLinkPath(): String = js.native
+  def historyAPIUpdateMethod(): String = js.native
 }
